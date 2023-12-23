52691aa4e6b5d3bd2fb71f3c0e0d599a0331804d
Yuval Dolev <mail@ydolev.com>
2023-12-24 01:31:00 +0200

    feat: receive a number as a CLI parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author = "ydolev", version = "0.1.0", about = "A minimalist C compiler written in Rust", long_about = None)]
+struct Cli {
+    number: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Number: {}", cli.number);
 }
